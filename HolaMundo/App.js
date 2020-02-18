f70141739b0d19c273397cad4f5774d5bb89627e
David Rodriguez Miranda <david@MacBook-Air-de-David.local>
2020-02-18 21:01:20 +0100

    Updates

--- HolaMundo/App.js
+++ HolaMundo/App.js
@@ -1,33 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-
-class Texto extends React.Component {
-  render() {
-    let { textoToMostrar } = this.props;
-
-    return <Text>{textoToMostrar}</Text>;
-  };
-}
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.fetchUsers();
+  }
+
   state = {
-    text: "",
+    loading: true,
+    users: [],
   };
 
-  handleChange = text => {
-    this.setState({ text });
+  fetchUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const pre = await response.json();
+    const users = pre.map( user => ({ ...user, key: String(user.id) }) );
+    this.setState({
+      loading: false,
+      users,
+    });
   }
 
   render() {
+    const { loading, users } = this.state;
+
+    const loadingText = (<Text>Loading...</Text>);
+
+    const FlatListVar = (
+      <FlatList 
+        data={users}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
+    );
+
     return (
       <View style={styles.container}>
-        <Texto textoToMostrar="Hi there!" />
-        <TextInput 
-          placeholder="Tell me something"
-          onChangeText={this.handleChange}
-        />
-        <Texto textoToMostrar={this.state.text} />
+        {loading ? loadingText : FlatListVar}
       </View>
     );
   }
@@ -36,9 +46,10 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    alignItems: 'center',
+    paddingTop: 80,
+  }
 });
